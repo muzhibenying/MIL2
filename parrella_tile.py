6bd0dff4c5205c5727f8f3ad531941c56d98b57b
muzhibenying <31839531+muzhibenying@users.noreply.github.com>
2019-09-01 20:59:25 +0800

    Update parrella_tile.py

--- parrella_tile.py
+++ parrella_tile.py
@@ -19,6 +19,8 @@ def simple_tile(image_path, resolution):
     print('resolution = ', resolution)
     slide_width = slide.level_dimensions[resolution][0]
     slide_height = slide.level_dimensions[resolution][1]
+    print('slide_width = ', slide_width)
+    print('slide_height = ', slide_height)
     grid_number_x = slide_width // grid_size + 1
     grid_number_y = slide_height // grid_size + 1
     image_list = []
@@ -28,15 +30,26 @@ def simple_tile(image_path, resolution):
             starty = i * grid_size
             tile_width = grid_size
             tile_height = grid_size
-            if (i + 1) * grid_size > slide_height:
-                startx = slide_height - grid_size
-            if (j + 1) * grid_size > slide_width:
-                starty = slide_width - grid_size
+            if startx + tile_width > slide_width:
+                startx = slide_width - tile_width - 1
+            if starty + tile_height > slide_height:
+                starty = slide_height - tile_height - 1
             if i * grid_size != slide_height and j * grid_size != slide_width:
                 tile = np.array(slide.read_region((startx * 2 ** resolution, starty * 2 ** resolution), 
                                                    resolution, (tile_width, tile_height)))
                 image_list.append(tile)
     return image_list
+
+def plot_grids_in_image(image, grid, grid_width, grid_height):
+    startx = grid[0]
+    starty = grid[1]
+    endx = startx + grid_width
+    endy = starty + grid_height
+    image[starty, startx: endx] = (0, 0, 0, 255)
+    image[endy, startx: endx] = (0, 0, 0, 255)
+    image[starty: endy, startx] = (0, 0, 0, 255)
+    image[starty: endy, endx] = (0, 0, 0, 255)
+    return image
 
 def convert_gray(image_list):
     # convert a colour image to a gray one, because original image is too large
@@ -51,7 +64,11 @@ def cal_var(histogram, threshold):
     back = histogram[histogram < threshold]
     fore = histogram[histogram >= threshold]
     var_back = back.var()
+    if back.shape[0] == 0:
+        var_back = 0
     var_fore = fore.var()
+    if fore.shape[0] == 0:
+        fore.back = 0
     var = (var_back * back.shape[0] + var_fore * fore.shape[0]) / (back.shape[0] + fore.shape[0])
     end = time.time()
     return var
@@ -68,9 +85,23 @@ def select_thre(image_list):
     thre = var.index(minvar) + 120
     return thre
 
+def very_simple_tile(image_path):
+    slide = OpenSlide(image_path)
+    res = slide.level_count - 1
+    whole_image = np.array(slide.read_region((0, 0), res, slide.level_dimensions[res]))
+    whole_image = cv2.cvtColor(whole_image, cv2.COLOR_BGR2GRAY)
+    histogram = whole_image.flatten()
+    var = []
+    for i in range(256):
+        if i != 0:
+            var.append(cal_var(histogram, i))
+    minvar = min(var)
+    thre = var.index(minvar)
+    return thre
+
 def select_grids(threshold, image_path, resolution):
     # use the original image to get the grid contain tissues
-    grids = []
+    now_grids = []
     slide = OpenSlide(image_path)
     slide_width = slide.level_dimensions[resolution][0]
     slide_height = slide.level_dimensions[resolution][1]
@@ -80,79 +111,43 @@ def select_grids(threshold, image_path, resolution):
         for j in range(grid_number_x):
             startx = j * grid_size
             starty = i * grid_size
-            tile_width = grid_size
-            tile_height = grid_size
-            if (i + 1) * grid_size > slide_height:
-                starty = slide_height - grid_size
-            if (j + 1) * grid_size > slide_width:
-                startx = slide_width - grid_size
+            if starty + grid_size > slide_height:
+                starty = slide_height - grid_size - 1
+            if startx + grid_size > slide_width:
+                startx = slide_width - grid_size - 1
             if i * grid_size != slide_height and j * grid_size != slide_width:
                 tile = np.array(slide.read_region((startx * 2 ** resolution, starty * 2 ** resolution),
-                                                   resolution, (tile_width, tile_height)))
+                                                   resolution, (grid_size, grid_size)))
                 gray_tile = cv2.cvtColor(tile, cv2.COLOR_BGR2GRAY)
                 no_background = gray_tile < threshold
-                if sum(sum(no_background)) / (grid_size * grid_size) > 0.1:
+            if sum(sum(no_background)) / (grid_size * grid_size) > 0.1:
                     grid = (startx, starty)
-                    grids.append(grid)
-    return grids
+                    now_grids.append(grid)
+    return now_grids
 
-def read_and_process(list_of_image_path, slides, grids, targets, mult, level):
-    for image_path in list_of_image_path:
-        image_list = simple_tile(image_path, 7)
-        gray_image_list = convert_gray(image_list)
-        threshold = select_thre(gray_image_list)
-        grid = select_grids(threshold, image_path, 7)
-        grids.append(grid)
+def create_lib(image_directory, category, resolution):
+    slides = []
+    grids = []
+    targets = []
+    mult = []
+    level = []
+    for image_path in os.listdir(image_directory):
+        image_path = '/notebooks/19_ZZQ/CAMELYON16_data/train_data/' + image_path
         slides.append(image_path)
+        threshold = very_simple_tile(image_path)
+        now_grids = select_grids(threshold, image_path, resolution)
+        print('now_grids = ', now_grids)
+        grids.append(now_grids)
         if 'tumor' in image_path:
             targets.append(1)
-        else:
+        if 'normal' in image_path:
             targets.append(0)
         mult.append(0)
-        level.append(0)
-        print(image_path + ' has precessed')
-
-def create_trainlib(image_directory):
-    slides = multiprocessing.Manager().list()
-    grids = multiprocessing.Manager().list()
-    targets = multiprocessing.Manager().list()
-    mult = multiprocessing.Manager().list()
-    level = multiprocessing.Manager().list()
-    file_list = os.listdir(image_directory)
-    list_of_image_path = []
-    every_process_num = len(file_list) // 48
-    for i in range(47):
-        every_list = file_list[i * every_process_num : (i + 1) * ever_process_num]
-        list_of_image_path.append(every_list)
-    last_list = [46 * every_process_num : ]
-    p = Pool(48)
-    for i in range(48):
-        p.apply_async(read_and_process, args=(list_of_image_path[i], slides, grids, targets, mult, level))
-    p.close()
-    p.join()
-    dictory = {'slides': slides, 'grid': grids, 'targets': targets, 'mult': mult, 'level': level}
-    torch.save(dictory, 'train_lib.pki')
-
-def create_vallib(image_director):
-    slides = multiprocessing.Manager().list()
-    grids = multiprocessing.Manager().list()
-    targets = multiprocessing.Manager().list()
-    mult = multiprocessing.Manager().list()
-    level = multiprocessing.Manager().list()
-    file_list = os.listdir(image_directory)
-    list_of_image_path = []
-    every_process_num = len(file_list) // 48
-    for i in range(47):
-        every_list = file_list[i * every_process_num : (i + 1) * ever_process_num]
-        list_of_image_path.append(every_list)
-    last_list = [46 * every_process_num : ]
-    p = Pool(48)
-    for i in range(48):
-        p.apply_async(read_and_process, args=(list_of_image_path[i], slides, grids, targets, mult, level))
-    p.close()
-    p.join()
-    dictory = {'slides': slides, 'grid': grids, 'targets': targets, 'mult': mult, 'level': level}
-    torch.save(dictory, 'val_lib.pki')
-
-create_trainlib('/notebooks/19_ZZQ/CAMELYON16_data/train_data')
-create_vallib('/notebooks/19_ZZQ/CAMELYON16_data/val_data')
+        level.append(resolution)
+    dictory = {'slides': slides, 'grids': grids, 'targets': targets, 'mult': mult, 'level': level}
+    torch.save(dictory, category + '_' + str(resolution) + '.pki')
+train_image_directory = '/notebooks/19_ZZQ/CAMELYON16_data/train_data'
+val_image_directory = '/notebooks/19_ZZQ/CAMELYON16_data/train_data'
+create_lib(train_image_directory, 'train', 8)
+create_lib(val_image_directory, 'val', 8)
+print('completed')
